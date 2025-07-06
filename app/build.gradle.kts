9add64339e1616c6d8b8e947aa3fccdd93feed64
hossein masoumi <218961604+hosseinmasoumi@users.noreply.github.com>
2025-07-07 00:11:33 +0330

    file update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
+    implementation(libs.android.lottie)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
