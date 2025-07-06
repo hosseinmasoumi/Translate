6c5d4078f5e1b94d91f274e66c47a0209c90d04f
hossein masoumi <218961604+hosseinmasoumi@users.noreply.github.com>
2025-07-06 23:09:58 +0330

    file up

--- app/src/main/java/com/example/translate/screens/Splashscsreen.kt
+++ app/src/main/java/com/example/translate/screens/Splashscsreen.kt
@@ -3,29 +3,27 @@ package com.example.translate.screens
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.translate.R
 
-class Splashscsreen : Fragment() {
+class SplashFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_splash_screen,
-            container, false)
+        return inflater.inflate(R.layout.fragment_splashscsreen, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         Handler(Looper.getMainLooper()).postDelayed({
-            // رفتن به صفحه اصلی بعد ۲ ثانیه
-
-
-        }, 2000)
+            findNavController().navigate(R.id.action_splashscsreen_to_mainpage)
+        }, 4000)
     }
 }
