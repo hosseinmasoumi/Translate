499f342cb0cfd21794bf864cff2bde26b36ea437
hossein masoumi <218961604+hosseinmasoumi@users.noreply.github.com>
2025-07-06 22:38:06 +0330

    file up

---
+++ app/src/main/java/com/example/translate/screens/Splashscsreen.kt
@@ -0,0 +1,31 @@
+package com.example.translate.screens
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.translate.R
+
+class Splashscsreen : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_splash_screen,
+            container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            // رفتن به صفحه اصلی بعد ۲ ثانیه
+
+
+        }, 2000)
+    }
+}
